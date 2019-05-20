61748d8d14aa130d2a67873a93b19dd0365be4bb
cjr8020 <cjr8020@gmail.com>
2019-05-20 11:03:21 -0700

    create changelog set

--- src/test/resources/db/changelog/db.changelog-1.0.sql
+++ src/test/resources/db/changelog/db.changelog-1.0.sql
@@ -1,4 +1,13 @@
-/*
- * CREATE SCHEMA SIC
- */
-CREATE USER TEST IDENTIFIED BY ABCD
+--liquibase formatted sql
+
+--changeset changelog:1.0
+
+
+
+-- CREATE SCHEMA TEST
+
+CREATE USER TEST IDENTIFIED BY "secret";
+
+GRANT UNLIMITED TABLESPACE TO TEST;
+
+
